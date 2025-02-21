5424eaf5e08dfa68ca213c2a0abf0724c111de88
KyleHEng <kylehussaineng@gmail.com>
2025-02-21 15:40:24 +0000

    Add feature to sort articles

--- src/components/Articles/ArticleList.jsx
+++ src/components/Articles/ArticleList.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import { articlesFetched } from "../axiosVariables";
 import ArticleListCard from "./ArticleListCard";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 function ArticleList() {
   const [articles, setArticles] = useState(false);
   const [limitAndPage, setLimitAndPage] = useState({ limit: 10, p: 1 });
   const { topic } = useParams();
-
+  const [isSortByOpen, setIsSortByOpen] = useState(false);
+  const [sortBy, setSortBy] = useState(undefined);
+  const [order, setOrder] = useState(undefined);
+  const [isOrderOpen, setIsOrderOpen] = useState(false);
   useEffect(() => {
     setArticles(false);
-    articlesFetched("", { params: { ...limitAndPage, topic } }).then(
-      (response) => {
-        setArticles(response.data.articles);
-      }
-    );
-  }, [limitAndPage, topic]);
+    articlesFetched("", {
+      params: { ...limitAndPage, topic, sort_by: sortBy, order },
+    }).then((response) => {
+      setArticles(response.data.articles);
+    });
+  }, [limitAndPage, topic, sortBy, order]);
 
   if (articles === false) {
     return <p>Loading</p>;
@@ -32,6 +35,21 @@ function ArticleList() {
       >
         All articles
       </button>
+      <button onClick={() => setIsSortByOpen(!isSortByOpen)}>Sort By</button>
+      {isSortByOpen ? (
+        <ul className="sort-by-container">
+          <li onClick={() => setSortBy("created_at")}>Date</li>
+          <li onClick={() => setSortBy("comment_count")}>Comment Count</li>
+          <li onClick={() => setSortBy("votes")}>Votes</li>
+          <li onClick={() => setIsOrderOpen(!isOrderOpen)}>Order</li>
+          {isOrderOpen ? (
+            <>
+              <span onClick={() => setOrder("asc")}>Ascending</span>{" "}
+              <span onClick={() => setOrder("desc")}>Descending</span>
+            </>
+          ) : null}
+        </ul>
+      ) : null}
       <main>
         {articles.map((article) => {
           return (
